7ba6f62d0dbeb68b3b859f4457fe90cf747c644d
mdrokybulhasan100-collab <mdrokybulhasan100@gmail.com>
2026-08-06 11:22:48 +0600

    Delete src directory

--- src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-const BN_DIGITS = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"];
-
-export function toBn(num) {
-  return String(num).replace(/[0-9]/g, (d) => BN_DIGITS[d]);
-}
-
-export function shuffle(arr) {
-  return [...arr].sort(() => Math.random() - 0.5);
-}
